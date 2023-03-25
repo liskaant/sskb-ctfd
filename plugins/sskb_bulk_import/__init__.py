5a956f7e25cd2f1653a99fcd108513245736cee4
liskaant <liskaant@gmail.com>
2023-03-25 17:59:19 +0100

    Improve error reporting of bulk import

--- plugins/sskb_bulk_import/__init__.py
+++ plugins/sskb_bulk_import/__init__.py
@@ -1,4 +1,4 @@
-import os
+import os, traceback, json
 from flask_restx import Namespace, Api, Resource
 from flask import Blueprint, request
 
@@ -6,12 +6,11 @@ from CTFd.utils.decorators import admins_only
 from CTFd.models import db, Users, Fields
 from CTFd.schemas.users import UserSchema
 from CTFd.cache import clear_standings
-from CTFd.api.v1.users import UserPublic
 from CTFd.utils.plugins import override_template
 
 from . import views
 
-api_namespace = Namespace('bulk import')
+api_namespace = Namespace('bulk_import')
 
 @api_namespace.route('')
 class BulkImport(Resource):
@@ -31,46 +30,59 @@ class BulkImport(Resource):
 		req = request.get_json()
 
 		if 'users' not in req or type(req['users']) != list:
-			return {'success': False, 'errors': 'Invalid input format'}, 400
+			return {'success': False, 'message': 'Invalid input format, expected array of users'}, 400
 
-		created_users = []
-		for user in req['users']:
-			if 'email' in user and user['email']:
-				Users.query.filter_by(email=user['email']).delete()
+		row_hint = "Hint: Row #0 is the header, first record is Row #1"
 
-			fields = []
+		created_users = 0
+		for i, row in enumerate(req['users']):
+			try:
+				if 'email' in row and row['email']:
+					Users.query.filter_by(email=row['email']).delete()
+				else:
+					return { "success": False, "message": f"Error processing Row #{i + 1}: Email is required\n{row_hint}\nRow dump: {json.dumps(row, indent=4)}" }, 400
 
-			if 'fields' in user and type(user['fields']) == dict:
-				for k, v in user['fields'].items():
-					field = Fields.query.filter_by(name=k).first()
-					if field:
-						fields.append({
-							'field_id': field.id,
-							'value': v,
-						})
+				fields = []
 
-			schema = UserSchema()
-			user = schema.load({
-				'name': user['name'] if 'name' in user else None,
-				'email': user['email'] if 'email' in user else None,
-				'password': user['password'] if 'password' in user else None,
-				'fields': fields,
-			})
+				if 'fields' in row and type(row['fields']) == dict:
+					for k, v in row['fields'].items():
+						field = Fields.query.filter_by(name=k).first()
+						if field:
+							fields.append({
+								'field_id': field.id,
+								'value': v,
+							})
 
-			if user.errors:
-				return {'success': False, 'errors': user.errors}, 400
+				schema = UserSchema()
+				user = schema.load({
+					'name': row['name'] if 'name' in row else None,
+					'email': row['email'] if 'email' in row else None,
+					'password': row['password'] if 'password' in row else None,
+					'fields': fields,
+				})
 
-			db.session.add(user.data)
-			created_users.append(schema.dump(user.data))
+				if user.errors:
+					db.session.rollback()
+					return {'success': False, 'message': f"Error processing Row #{i + 1}\n{row_hint}\nError: {json.dumps(user.errors, indent=4)}\nRow dump: {json.dumps(row, indent=4)}" }, 400
 
-		db.session.commit()
-		clear_standings()
+				db.session.add(user.data)
+				created_users += 1
+			except:
+				db.session.rollback()
+				return { 'success': False, 'message': f"Error processing Row #{i + 1}\nHint: Row #0 is the header, first record is Row #1\nError: " + traceback.format_exc() + f"\nRow dump: {json.dumps(row, indent=4)}" }, 400
 
-		return { 'success': True }
+		try:
+			db.session.commit()
+			clear_standings()
+		except:
+			db.session.rollback()
+			return { 'success': False, 'message': f"Error commiting changes, please try again\nError: " + traceback.format_exc() }, 400
+
+		return { 'success': True, 'message': f'Ok: Created or updated {created_users} users' }
 
 def load(app):
 	dir_path = os.path.dirname(os.path.realpath(__file__))
-	override_template('sskb_bulk_import_admin.html', open(os.path.join(dir_path, 'sskb_bulk_import_admin.html')).read())
+	override_template('sskb_bulk_import_admin.html', open(os.path.join(dir_path, 'templates/sskb_bulk_import.html')).read())
 
 	api = Blueprint('bulk_import_api', __name__, url_prefix='/api/v1')
 	BulkImportApi = Api(api, version='v1', doc=app.config.get('SWAGGER_UI'))
